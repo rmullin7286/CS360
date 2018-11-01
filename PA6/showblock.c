794aa682967bd8e2df549dd571a46dd7b7edd5f2
Ryan Mullin <ryan.mullin@wsu.edu>
2018-11-01 16:42:11 -0700

    finished PA6

--- PA6/showblock.c
+++ PA6/showblock.c
@@ -3,6 +3,7 @@
 #include <ext2fs/ext2_fs.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdint.h>
 
 #define SUPERBLOCK_MAGIC_NUMBER 0xEF53
 #define BLKSIZE 1024
@@ -21,9 +22,72 @@ void get_block(int fd, long blk, char buf[])
     read(fd, buf, BLKSIZE);
 }
 
+void print_indirect_blocks(char dbuf[])
+{
+    int line = 0;
+    for(char* cur = dbuf; cur < dbuf + BLKSIZE; cur += 4)
+    {
+        uint32_t val = *((uint32_t*)cur);
+        if(val == 0)
+        {
+            printf("\n");
+            return;
+        }
+        printf("%d ", val);
+        if(++line == 8)
+        {
+            printf("\n");
+            line = 0;
+        }
+    }
+    printf("\n");
+}
+
+void print_double_indirect_blocks(int fd, char dbuf[])
+{
+    for(char* cur = dbuf; cur < dbuf + BLKSIZE; cur += 4)
+    {
+        uint32_t val = *((uint32_t*)cur);
+        if(val == 0)
+        {
+            printf("\n");
+            return;
+        }
+        char dbuf2[BLKSIZE];
+        get_block(fd, val, dbuf2);
+        print_indirect_blocks(dbuf2);
+    }
+    printf("\n");
+}
+
+void print_blocks(int fd, const INODE *ip)
+{
+    for(int i = 0; i < 12; i++)
+    {
+        if(ip->i_block[i] == 0)
+            return;
+        printf("i_block[%d] = %d\n", i, ip->i_block[i]);
+    }
+
+    char dbuf[BLKSIZE];
+
+    if(ip->i_block[12] != 0)
+    {
+        get_block(fd, ip->i_block[12], dbuf);
+        printf("----------indirect blocks----------\n");
+        print_indirect_blocks(dbuf);
+    }
+
+    if(ip->i_block[13] != 0)
+    {
+        get_block(fd, ip->i_block[13], dbuf);
+        printf("----------double indirect blocks--------\n");
+        print_double_indirect_blocks(fd, dbuf);
+    }
+}
+
 int search(int dev, const INODE *ip, const char * name)
 {
-    printf("=========================================");
     printf("i_number    rec_len    name_len    name\n");
     char dbuf[BLKSIZE], temp[256];
     DIR* dp;
@@ -84,8 +148,9 @@ int main(int argc, char* argv[])
     //get inode table
     printf("Retrieving inode table...");
     get_block(dev, GDBLK, buf);
-    get_block(dev, ((GD*)buf)->bg_inode_table, buf);
-    printf("DONE\n");
+    int iblk = ((GD*)buf)->bg_inode_table;
+    get_block(dev, iblk, buf);
+    printf("DONE\n\n");
 
     //retrieve root dir
     INODE* ip = (INODE*)buf + 1;
@@ -95,18 +160,29 @@ int main(int argc, char* argv[])
     char * path = argv[2];
     if(path[0] == '/')
         path++;
-    char * tok = strtok(path, '/');
+    char * tok = strtok(path, "/");
+    int num;
     do
     {
-        printf("Searching %s for %s... ", current_dir, tok);
-        int num = search(dev, ip, tok);
+        printf("=========================\n");
+        printf("Searching %s for %s... \n", current_dir, tok);
+        num = search(dev, ip, tok);
         if(num == 0)
         {
             printf("NOT FOUND\n");
             return 0;
         }
-        printf("FOUND\n");
-    }while(tok = strtok(NULL, '/'));
+        printf("FOUND\n\n");
+        strcpy(current_dir, tok);
+        
+        int blk = (num - 1) / 8 + iblk;
+        int offset = (num - 1) % 8;
+        get_block(dev, blk, buf);
+        ip = (INODE*)buf + offset;
+    }while(tok = strtok(NULL, "/"));
 
+    printf("==========================\n");
+    printf("Found %s: ino = %d\n", current_dir, num);
+    print_blocks(dev, ip);
     return 0;
 }
